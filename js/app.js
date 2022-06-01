6dc4fcf8a873e8cf7b00a92f30cefa2290734b9e
nathanth <nathan.hardenburg@icloud.com>
2022-06-01 14:44:12 -0500

    uses event listener to add li to body based on user input to text box

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('sanity check')
+const inpt = document.getElementById("user-input")
+const btn = document.getElementById("submit-button")
+const todoList = document.getElementById("todo-list")
+
+
+btn.addEventListener("click", function(evt) {
+  const li = document.createElement('li')
+  li.textContent = inpt.value
+  todoList.appendChild(li)
+  inpt.value = ''
+})
+
