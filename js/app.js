9baabc32d35749ccd149b442cd8dd4738242dfb4
nathanth <nathan.hardenburg@icloud.com>
2022-06-01 14:50:29 -0500

    if statement prevents user from adding blank to the list

--- js/app.js
+++ js/app.js
@@ -4,9 +4,13 @@ const todoList = document.getElementById("todo-list")
 
 
 btn.addEventListener("click", function(evt) {
-  const li = document.createElement('li')
-  li.textContent = inpt.value
-  todoList.appendChild(li)
-  inpt.value = ''
+  if (inpt.value === ''){
+    alert('There is nothing to add in the text box.')
+  } else {
+    const li = document.createElement('li')
+    li.textContent = inpt.value
+    todoList.appendChild(li)
+    inpt.value = ''
+  }
 })
 
